5ca7edafc9c7619b006fa89812e17d309af1b655
mridul <mridul.choudhary7@gmail.com>
2022-06-19 13:22:35 +0530

    1st commit

---
+++ src/main/java/com/mcy5/HelloWorld/Controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.mcy5.HelloWorld.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+    @GetMapping("/v1/greet")
+    public ResponseEntity<?> greeting(){
+        return ResponseEntity.ok("Request success !");
+    }
+}
